e94ea66109e21a56f0525e09b740dcda4816143d
Frédéric Libaud <frederic.libaud@numx-sets.eu>
2023-09-18 11:42:21 +0200

    initial code

---
+++ Packages/PkgLibDuckDB.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Typhon IDE. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit PkgLibDuckDB;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  DuckDBAPI, TyphonPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('PkgLibDuckDB', @Register);
+end.
