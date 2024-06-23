9828431642f05ed9a1c596301b76e482fcde8f08
Frédéric Libaud <frederic.libaud@numx-sets.eu>
2024-06-23 21:29:47 +0200

    Code adjoinction, correcting

---
+++ Sources/DuckDBAPIExceptions.pas
@@ -0,0 +1,15 @@
+unit DuckDBAPIExceptions;
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  EDuckDBAPI = class (Exception)
+
+    end;
+
+implementation
+
+end.
---
+++ Sources/DuckDBAPIRessources.pas
@@ -0,0 +1,11 @@
+unit DuckDBAPIRessources;
+
+interface
+
+resourcestring
+  SErrAlreadyLoaded = 'DuckDB client library (%s) are already loaded !';
+  SErrLoadFailed = 'Failed to load DuckDB client library (%s) !';
+
+implementation
+
+end.
